05d0f4f1df3a5fb2051864b1eb871a2581de7cf0
tccheungvtc <tccheungvtc@gmail.com>
2022-05-27 11:08:52 +0800

    Version 1.2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String a ="";
+        int i=12;
 
     }
 }
